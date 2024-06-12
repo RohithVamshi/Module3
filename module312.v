928626ff85b638fd2a7b799e8e111432af15ca18
RohithVamshi <161675614+RohithVamshi@users.noreply.github.com>
2024-06-12 14:54:00 +0530

    Update module312.v

--- module312.v
+++ module312.v
@@ -1,1 +1,14 @@
-m
+module module32
+#(parameter datawidth=120)
+
+(input [datawidth-1:0] a,
+input [datawidth-1:0] b, input [datawidth-1:0] c, 
+output reg [2*datawidth-1:0] result);
+
+always @(*)
+begin
+result = (a+b)*c;
+end
+
+endmodule
+
